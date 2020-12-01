ba93db41a38c92c6dc5e25701bafcf977a6fa2f0
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-12-01 11:08:37 +0700

    StopWatch OPP

---
+++ stopWatchOPP/index.php
@@ -0,0 +1,46 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+class StopWatch
+{
+    private $startTime;
+    private $endTime;
+
+    function __construct()
+    {
+        $this->startTime = time();
+    }
+
+    function getStartTime()
+    {
+        return $this->startTime;
+    }
+
+    function getEndTime()
+    {
+        return $this->endTime;
+    }
+
+    function start()
+    {
+        $this->startTime =  time();
+    }
+
+    function stop()
+    {
+        $this->endTime =  time();
+    }
+
+    function getElapsedTime()
+    {
+        return  $this->endTime - $this->startTime;
+    }
+}
+
+$watch = new StopWatch();
+$watch->start();
+
+file_get_contents('https://www.google.com/');
+
+$watch->stop();
+$kq = $watch->getElapsedTime();
+echo $kq;
