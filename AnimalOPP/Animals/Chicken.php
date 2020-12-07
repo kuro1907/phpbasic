a40ccf20afc12ea585cd459a3eafac368685d9a9
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-12-07 08:21:51 +0700

    Animal OPP

---
+++ AnimalOPP/Animals/Chicken.php
@@ -0,0 +1,18 @@
+<?php
+include_once(dirname(__FILE__) . '/../AbstractClass/Animal.php');
+include_once(dirname(__FILE__) . '/../InterfaceClass/Edible.php');
+
+class Chicken extends Animal implements Edible
+{
+    public function makeSound()
+    {
+        return "Chicken: cluck-cluck!";
+    }
+
+    public function howToEat()
+    {
+        return "could be fried";
+    }
+}
+
+var_dump(dirname(__FILE__));
