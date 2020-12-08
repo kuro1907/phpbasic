9f07e436d50c1676c54cfebc69adefb267271b9f
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-12-08 09:08:19 +0700

    bai tap circle Cylinder

---
+++ BT_CircleCylinder/Cylinder.php
@@ -0,0 +1,27 @@
+<?php
+include 'Circle.php';
+class Cylinder extends Circle
+{
+    public $height;
+
+    public function __construct($radius, $color, $height)
+    {
+        parent::__construct($radius, $color);
+        $this->height = $height;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+
+    public function calculateArea()
+    {
+        return parent::calculateArea() * 2 + parent::calculatePerimeter() * $this->height;
+    }
+}
