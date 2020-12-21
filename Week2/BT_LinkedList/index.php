a1bf69a5fb4ace8dc03128887ecec579fc88018d
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-12-21 08:22:29 +0700

    BT_Linkedlist

---
+++ Week2/BT_LinkedList/index.php
@@ -0,0 +1,51 @@
+<?php
+class LinkedList
+{
+    public $count = 0;
+    public $firstNode;
+    public $lastNode;
+
+    public function __construct()
+    {
+    }
+
+    public function add($index)
+    {
+    }
+
+    public function addFirst()
+    {
+    }
+
+    public function addLast()
+    {
+    }
+
+    public function remove($index)
+    {
+    }
+
+    public function get($index)
+    {
+    }
+
+    public function size()
+    {
+    }
+
+    public function printList()
+    {
+    }
+
+    public function clone()
+    {
+    }
+
+    public function contains($obj)
+    {
+    }
+
+    public function indexOf()
+    {
+    }
+}
