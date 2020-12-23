fbfb75497158af103d6ee52cd0ce1adc4a3a907e
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-12-23 08:23:03 +0700

    find Min Max

---
+++ Week2/findMinMax/index.php
@@ -0,0 +1,40 @@
+<?php
+function findMin($arr)
+{
+    $min = $arr[0];
+    for ($i = 1; $i < count($arr); ++$i) {
+        if ($arr[$i] < $min) {
+            $min = $arr[$i];
+        }
+    }
+    return $min;
+}
+function findMax($arr)
+{
+    $max = $arr[0];
+    for ($i = 1; $i < count($arr); ++$i) {
+        if ($arr[$i] > $max) {
+            $max = $arr[$i];
+        }
+    }
+    return $max;
+}
+
+$nums = [];
+for ($i = 0; $i < 100; ++$i) {
+    $nums[$i] = rand(1, 101);
+}
+foreach ($nums as $num) {
+    echo $num . " ";
+}
+
+$minValue = findMin($nums);
+echo "<br/>";
+echo "The Minimum value is: " . $minValue;
+
+$maxValue = findMax($nums);
+echo "<br/>";
+echo "The Maximum value is: " . $maxValue;
+
+echo "<br/>";
+echo "The Average value is: " . ($maxValue + $minValue) / 2;
